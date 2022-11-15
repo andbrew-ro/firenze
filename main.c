099a6c0f0ea3afe28db0a19f151036cd2fc37f5b
Andres Romero <andbrew.dev@proton.me>
2022-11-14 19:36:05 -0600

    43°46′17″N 11°15′15″E

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *count(char *s) {
+    int *x = calloc(26, sizeof(int));
+    for(int i = 0; i < strlen(s); i++) {
+        if(isblank(s[i])) continue;
+        x[toupper(s[i])-'A']++;
+    }
+    return x;
+}
+
+int check(char *a, char *b) {
+    int *x_a = count(a), *x_b = count(b);
+    for(int i = 0; i < 26; i++) {
+        if(x_a[i] != x_b[i])
+            return 0;
+    }
+    return 1;
+}
+
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        printf("2 args expected\n");
+        exit(1);
+    }
+    if(check(argv[1], argv[2])) 
+        printf("\"%s\" IS an anagram of \"%s\"\n", argv[2], argv[1]);
+    else
+        printf("\"%s\" is NOT an anagram of \"%s\"\n", argv[2], argv[1]);
+    return 0;
+}
